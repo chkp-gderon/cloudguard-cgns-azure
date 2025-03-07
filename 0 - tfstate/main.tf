c16539647fc9f9f22c96da9d20aa47c209edb09a
Jon Paine <jpaine@checkpoint.com>
2025-03-07 17:17:18 +0000

    Added NSG to this repo.

--- 0 - tfstate/main.tf
+++ 0 - tfstate/main.tf
@@ -4,15 +4,15 @@ resource "random_string" "resource_code" {
   upper   = false
 }
 
-resource "azurerm_resource_group" "tfstate" {
-  name     = "tfstate"
+resource "azurerm_resource_group" "this" {
+  name     = "tfstate-nsg-rg"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "tfstate" {
   name                     = "${random_string.resource_code.result}"
-  resource_group_name      = azurerm_resource_group.tfstate.name
-  location                 = azurerm_resource_group.tfstate.location
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   allow_nested_items_to_be_public = false
@@ -26,4 +26,49 @@ resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
   storage_account_name  = azurerm_storage_account.tfstate.name
   container_access_type = "private"
+}
+resource "azurerm_network_security_group" "this" {
+  resource_group_name = azurerm_resource_group.this.name 
+  location = azurerm_resource_group.this.location
+  name = "we-cp-dsb-nsg"
+}
+resource "azurerm_network_security_rule" "out" {
+  network_security_group_name = azurerm_network_security_group.this.name
+  resource_group_name = azurerm_resource_group.this.name
+  source_address_prefixes = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16","198.18.0.0/15"]
+  source_port_range = "*" 
+  name = "Allow-all-out"
+  access = "Allow"
+  protocol = "*"
+  destination_port_range = "*"
+  destination_address_prefix = "*"
+  direction = "Outbound"
+  priority = 110
+}
+resource "azurerm_network_security_rule" "in" {
+  network_security_group_name = azurerm_network_security_group.this.name
+  resource_group_name = azurerm_resource_group.this.name
+  
+  source_address_prefixes = concat(["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"], var.cp_addresses, var.fdb_addresses)
+  source_port_range = "*" 
+  name = "Allow-all-in"
+  access = "Allow"
+  protocol = "*"
+  destination_port_range = "*"
+  destination_address_prefix = "*"
+  direction = "Inbound"
+  priority = 110
+}
+resource "azurerm_network_security_rule" "ne-appgw-in" {
+  network_security_group_name = azurerm_network_security_group.this.name
+  resource_group_name = azurerm_resource_group.this.name
+  source_address_prefix = "*"
+  source_port_range = "*" 
+  name = "Allow-appgw-high-ports-in"
+  access = "Allow"
+  protocol = "*"
+  destination_port_range = "65200-65535"
+  destination_address_prefix = "*"
+  direction = "Inbound"
+  priority = 130
 }
---
+++ 0 - tfstate/variables.tf
@@ -0,0 +1,8 @@
+ variable "cp_addresses" {
+   type = list(string)
+   default = ["109.148.15.64/32"]
+}
+  variable "fdb_addresses" {
+   type = list(string)
+   default = ["239.255.255.255/32"]
+}

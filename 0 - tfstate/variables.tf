d9e95e127cc6d8f3c91259f4206957e29d158a66
Jon Paine <jpaine@checkpoint.com>
2025-03-11 15:39:21 +0000

    Updated NSG.

--- 0 - tfstate/variables.tf
+++ 0 - tfstate/variables.tf
@@ -1,6 +1,6 @@
  variable "cp_addresses" {
    type = list(string)
-   default = ["77.33.182.50/32"]
+   default = ["77.33.182.50/32", "109.148.15.64/32"]
 }
   variable "fdb_addresses" {
    type = list(string)
